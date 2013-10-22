687c446baaa5f687339d6f6f5295201498d814f4
Ryan Van Etten <ryanve@gmail.com>
2013-10-22 00:30:31 -0400

    Systemize tests.

--- test/tests.js
+++ test/tests.js
@@ -1,74 +1,146 @@
 (function(root, doc) {
-    var aok = root.aok
+    var fails = 0
+      , aok = root.aok
       , vibe = root.vibe
+      , effin = vibe.fn
+      , slice = [].slice
       , docElem = doc.documentElement
       , subject = docElem['classList']
-      , hasApi = !!(subject && subject.contains && subject.toggle && subject.add && subject.remove)
+      , hasApi = !!(subject && subject.contains && subject.add && subject.remove)
+      , invoke = function(method, e) {
+            var chain = +!e.nodeType, scope = chain ? e : this;
+            return (chain ? effin : vibe)[method].apply(scope, slice.call(arguments, 1+chain));
+        }
+      , partial = function(fn) {
+            var rest = slice.call(arguments, 1);
+            return function() {
+                return fn.apply(this, rest.concat(slice.call(arguments)));
+            };
+        }
+      , hasKlass = partial(invoke, 'hasClass')
       , log = aok.log
       , info = aok.info
-        // emulate wrapped nodes:
       , $divs = doc.getElementsByTagName('div')
-      , $body = [doc.body];
+      , cull = aok.prototype.cull
+      , borrow = function(e, fn, init) {
+            var r, a = e.nodeType ? [e] : e, l = a.length, i = l, j = l, before = [];
+            init = init || '';
+            while (j--) before[j] = a[j].className;
+            while (i--) a[i].className = init;
+            r = fn.call(this, e, init);
+            while (l--) a[l].className = before[l];
+            return r;
+        };
+
+    aok.prototype.cull = function() {
+        this.test || fails++;
+        return cull.apply(this, arguments);
+    };
 
     log(vibe);
-    info('native: ' + hasApi);
+    info('hasNative: ' + hasApi);
     aok.prototype.express = info;
+    
+    aok(function() {
+        var sub = vibe[this.id='hasClass'];
+        return borrow(doc.body, function(e, c) {
+            return true === sub(e, c);
+        }) && borrow(doc.body, function(e, c) {
+            if (c[1] && sub(e, c.slice(1))) return;
+            return true === sub(e, c) && false === sub(e, 'red');
+        }, 'blue');
+    });
+    
+    aok(function() {
+        var sub = vibe[this.id='addClass'];
+        return borrow(doc.body, function(e, c) {
+            sub(e, c);
+            if (true !== hasKlass(e, c)) return;
+            sub(e, 'red');
+            return true === hasKlass(e, 'red');
+        });
+    });
+    
+    aok(function() {
+        var sub = vibe[this.id='removeClass'];
+        return borrow(doc.body, function(e, c) {
+            sub(e, c);
+            if (false !== hasKlass(e, c)) return;
+            sub(e, 'red');
+            return false === hasKlass(e, 'red');
+        }, 'blue');
+    });
+    
+    aok(function() {
+        var sub = vibe[this.id='toggleClass'];
+        return borrow(doc.body, function(e, c) {
+            return true === sub(e, c) && true === sub(e, c, true) && true === sub(e, c, false);
+        }) && borrow(doc.body, function(e, c) {
+            var i, before = hasKlass(e, c), bools = [sub(e, c), sub(e, c), sub(e, c)];
+            if (before === bools[0]) return;
+            if (before !== bools[1]) return;
+            for (i = bools.length; i--;) { 
+                if (typeof bools[i] != 'boolean') return;
+                if (bools[i] !== sub(e, c, bools[i])) return;
+                if (bools[i] !== hasKlass(e, c)) return;
+            }
+            return true;
+        }, 'blue');
+    });
+    
+    aok(function() {
+        var sub = vibe.fn[(this.id='.hasClass').slice(1)];
+        return borrow(doc.body, function(e, c) {
+            return true === sub.call([e], c);
+        }) && borrow(doc.body, function(e, c) {
+            if (c[1] && sub([e], c.slice(1))) return;
+            return true === sub.call([e], c);
+        }, 'green') && borrow(doc.body, function(e) {
+            return true === sub.call([e], 'red') && false === sub.call([e], 'green');
+        }, 'blue red');
+    });
+    
+    aok(function() {
+        var sub = vibe.fn[(this.id='.addClass').slice(1)];
+        return borrow($divs, function($e) {
+            return $e === sub.call($e, 'red') && hasKlass($e, 'red');
+        }) && borrow($divs, function($e, c) {
+            return $e === sub.call($e, c) && hasKlass($e, c);
+        }, 'red') && borrow($divs, function($e) {
+            var list = ['red', 'white', 'blue'], i = list.length;
+            sub.call($e, list.join(' '));
+            while (i--) if (!hasKlass($e, list[i])) return;
+            return true;
+        });
+    });
 
-    aok({id:'green', test:function() {
-        doc.body.className = 'green';
-        return true === vibe.fn.hasClass.call($body, 'green') &&
-            $body === vibe.fn.removeClass.call($body, 'green') &&
-            false === vibe.fn.hasClass.call($body, 'green');
-    }});
+    aok(function() {
+        var sub = vibe.fn[(this.id='.removeClass').slice(1)];
+        return borrow($divs, function($e) {
+            return $e === sub.call($e, 'red') && !hasKlass($e, 'red');
+        }) && borrow($divs, function($e, c) {
+            return $e === sub.call($e, c) && !hasKlass($e, c);
+        }, 'red') && borrow($divs, function($e) {
+            var list = ['red', 'white', 'blue'], i = list.length;
+            sub.call($e, list.join(' '));
+            while (i--) if (hasKlass($e, list[i])) return;
+            return true;
+        });
+    });
+    
+    aok(function() {
+        var sub = vibe.fn[(this.id='.toggleClass').slice(1)];
+        return borrow($divs, function($e, c) {
+            return $e === sub.call($e, c) && !hasKlass($e, c);
+        }, 'red') && borrow($divs, function($e) {
+            return $e === sub.call($e, 'red') && hasKlass($e, 'red');
+        }) && borrow($divs, function($e, c) {
+            var bools = [true, false, true], i = bools.length;
+            while (i--) if ($e !== sub.call($e, c, bools[i]) || hasKlass($e, c) !== bools[i]) return;
+            return true;
+        }, 'red');
+    });
 
-    aok({id:'magenta', test:function() {
-        return false === vibe.fn.hasClass.call($body, 'magenta') && 
-            $body === vibe.fn.addClass.call($body, 'magenta') &&
-            true === vibe.fn.hasClass.call($body, 'magenta');
-    }});
-
-    aok({id:'red', test:function() {
-        return false === vibe.fn.hasClass.call($body, 'red') &&
-            $body === vibe.fn.toggleClass.call($body, 'red') &&
-            true === vibe.fn.hasClass.call($body, 'red');
-    }});
-
-    aok({id:'yellow', test:function() {
-        vibe.addClass(doc.body, 'yellow');
-        return true === vibe.hasClass(doc.body, 'yellow') &&
-            false === vibe.toggleClass(doc.body, 'yellow') &&
-            false === vibe.hasClass(doc.body, 'yellow') && 
-            true === vibe.toggleClass(doc.body, 'yellow') &&
-            true === vibe.hasClass(doc.body, 'yellow');
-    }});
-
-    aok({id:'addMulti', test:function() {
-        return $divs === vibe.fn.removeClass.call($divs, 'white magenta') &&
-            false === vibe.fn.hasClass.call($divs, 'white') &&
-            false === vibe.fn.hasClass.call($divs, 'magenta') &&
-            $divs === vibe.fn.addClass.call($divs, 'white magenta') &&
-            true === vibe.fn.hasClass.call($divs, 'white') && 
-            true === vibe.fn.hasClass.call($divs, 'magenta');
-    }});
-
-    aok({id:'remFunky', test:function() {
-        return $divs === vibe.fn.removeClass.call($divs, function () { return 'white'; });
-    }});
-
-    aok({id:'white', test:function() {
-        var token = 'white'
-          , toggle = vibe.fn.toggleClass
-          , has = vibe.fn.hasClass
-          , before = has.call($divs, 'white')
-          , bools = [true, false, true]
-          , i = bools.length;
-        if (typeof before != 'boolean') return false;
-        if ($divs !== toggle.call($divs, token)) return false;
-        if (before === has.call($divs, token)) return false;
-        while (i--) {
-            vibe.fn.toggleClass.call($divs, token, bools[i]);
-            if (bools[i] !== vibe.fn.hasClass.call($divs, token)) return false;
-        }
-        return true;
-    }});
+    vibe.toggleClass(docElem, 'failed', !!fails);
+    vibe.toggleClass(docElem, 'passed', !fails);
 }(this, document));
